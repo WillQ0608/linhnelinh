c33e57ea877df476639090a394d5ef1dd38c2b5e
WillQ0608 <73808996+WillQ0608@users.noreply.github.com>
2021-06-16 03:14:03 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Linhh!!',
+    introDesc: `Trễ lắm rồi, sao hôm nào cũng hại bản thân vậy`,
+    btnIntro: 'Em thích thức khuya đấy thì sao!',
+    title: 'Em đã quên cho bản thân sống tích cực và ngủ đúng giờ >< ?',
+    desc: 'Phải chăng em đã quen với việc hại bản thân tới 6h sáng r ngủ tới 1 giờ chiều ',
+    btnYes: 'ok e đi ngủ nè, 15p cuối nha <3',
+    btnNo: 'Tại em thích vậy đó !',
+    question:'Chịu đi ngủ sớm là ngoan đó, vậy mới tích cực được!',
+    btnReply: 'Cám ơn ha!',
+    reply: 'Ngủ ngon nha',
+    mess: 'Ngoan đó!',
+    messDesc: 'Chúc em luôn vui.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
